878691aa06282c18f560af16f1c02427a1c56685
Calvin-OBS <calvin.govindsamy@oceanbluesoftware.co.uk>
2023-06-20 00:23:08 +0200

    added user promt to enter age

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,4 @@
-condole.log('GIT  is awesome')
+console.log('GIT  is awesome');
+const userInput=promt("Enter your age");
+
+isNaN(userInput)==true? console.log("Did not eneter a number"):console.log("user age is "+userInput)
